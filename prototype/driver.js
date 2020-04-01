a2a864c5eb408acf41ed81a67db87cd93d51858e
OhadR <ohadr.developer@gmail.com>
2020-04-01 18:51:47 +0300

    npm install di4js - work with data injection

--- prototype/driver.js
+++ prototype/driver.js
@@ -1,37 +1,30 @@
-function Engine(hp) {
-    this.hp = hp;
+di = require('di4js');
+
+function Engine() {
 }
 
 Engine.prototype.start = function () {
-    console.log("Engine with " + this.hp + " hp has been started...");
+    console.log("Engine has been started...");
 }
 
-function Car(name, engine) {
-    this.name = name;
-    this.engine = engine;
+function Car() {
+    this.engine = null;
 }
 
 Car.prototype.start = function () {
     if (this.engine) {
         this.engine.start();
     }
+
+    console.log("Car has been started...");
 }
 
-function Driver(name, car) {
-    this.name = name;
-    this.car = car;
-}
+di
+    .autowired(true)
+    .register('engine')
+    .as(Engine)
+    .register('car')
+    .as(Car);
 
-Driver.prototype.drive = function () {
-    if (this.car) {
-        this.car.start();
-    }
-}
-
-var driver = Driver("tom");
-
-driver.car = new Car("wv", new Engine(256)));
-
-driver.drive();
-
-// Engine with 256 hp has been started...
+var car = di.resolve('car');
+car.start();
