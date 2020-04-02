861e7049eb4cf005e780e21b7027cadb384fae2f
OhadR <ohadr.developer@gmail.com>
2020-04-02 12:04:29 +0300

    promises - understand resolve/reject

---
+++ promise/testPromise.js
@@ -0,0 +1,30 @@
+/**
+ * note that in someFunction(), even after resolve() is called, the flow continues and all logs
+ * to console are printed. The Promise is resolved, and the 'reject' is ignored, but it is invoked.
+ * if we want to avoid that, we should call 'return resolve()', not just 'resolve()'
+ * @returns {Promise<unknown>}
+ */
+
+function someFunction() {
+    return new Promise( function(resolve, reject) {
+        console.log('before resolveing')
+        resolve(123);
+        console.log('after resolveing')
+        reject(456)
+        console.log('after rejecting')
+    })
+}
+
+
+function caller() {
+
+    someFunction()
+    .then( (retVal) => {
+        console.log("retval = " + retVal)
+    })
+    .catch( (err) => {
+        console.error('ohads ERROR: ' + err)
+    })
+}
+
+caller()
